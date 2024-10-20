753e5d14768b02f0c03822601c6722f65706462e
Alexandre Landi <alexandre.landi@skema.edu>
2024-10-20 23:37:15 +0200

    substitute logarithmic returns with arithmetic ones

--- l1-measuring-returns/exercise-1/solution/1.3_solution.py
+++ l1-measuring-returns/exercise-1/solution/1.3_solution.py
@@ -4,23 +4,19 @@ import yfinance as yf
 from utils import plot_rolling_mean
 
 
-# Downloading S&P 500 futures data
-ticker = "ES=F"  # S&P 500 front-month futures ticker symbol
-data = yf.download(ticker)["Adj Close"]
-
 # Creating a pandas dataframe
-sp500_data = pd.DataFrame(data)
+prices = pd.read_csv("data/sp500.csv", index_col=0, parse_dates=True)
 
 # Computing log returns
-log_returns = np.log(sp500_data).diff()
+returns = prices.pct_change()
 
 # Calculating cumulative returns
-cumulative_returns = np.exp(np.cumsum(log_returns))
+cumulative_returns = (1 + returns).cumprod()
 
 # Calculating rolling annualized mean return
-rolling_mean_1yr = log_returns.rolling(252).mean() * 252
-rolling_mean_3yrs = log_returns.rolling(756).mean() * 252
-rolling_mean_10yrs = log_returns.rolling(2520).mean() * 252
+rolling_mean_1yr = returns.rolling(252).mean() * 252
+rolling_mean_3yrs = returns.rolling(756).mean() * 252
+rolling_mean_10yrs = returns.rolling(2520).mean() * 252
 
 plot_rolling_mean(cumulative_returns, rolling_mean_1yr)
 plot_rolling_mean(cumulative_returns, rolling_mean_3yrs)
